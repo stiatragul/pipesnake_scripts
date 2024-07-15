ac3f5b5552c3d029fdadf0a028b9dcf6d943b3d8
puttert <stiatragul@live.com>
2024-07-15 21:31:25 +1000

    clean up install script

--- R_scripts/install_biocmanager.R
+++ R_scripts/install_biocmanager.R
@@ -3,17 +3,21 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
 BiocManager::install()
 
+### If installing on linux you might have to install other dependencies outside first
+install.packages(c("ragg", "pkgdown", "httr", "XML", "restfulr", "curl", "devtools"))
 
-install.packages(c("httr", "XML", "restfulr", "curl", "devtools"))
+.libPaths("~/R/x86_64-pc-linux-gnu-library/4.4")
+.libPaths()
 
 
 # install package dependencies
 BiocManager::install(
     c("UCSC.utils", "GenomeInfoDb", "GenomicRanges", "SummarizedExperiment", "Rhtslib", "Rsamtools", "KEGGREST", "AnnotationDbi", 
     "GenomicAlignments", "rtracklayer", "GenomicFeatures", "Biostrings"
-    )
+    ), lib = "~/R/x86_64-pc-linux-gnu-library/4.4"
 )
 
+library(devtools)
 
 # install.packages("devtools")
 # install the current version of metablastr on your system
